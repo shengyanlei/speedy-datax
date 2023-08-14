6eeed261470157dc8cafc54cdf8499adab9edc0d
shyl <s13934738165@163.com>
2023-08-14 15:47:08 +0800

    ”第一次提交“

---
+++ dataxCodeGeneration/main.py
@@ -0,0 +1,55 @@
+from dataxJson.jsonGeneration import mysql_table_info
+import json
+from datetime import datetime
+#自动生成datax-json
+def datax_json(table_name,type):
+    table_info = mysql_table_info(table_name)
+    hdfs_column=[]
+    for i in range(len(table_info[1])):
+        if(table_info[2][i][0:3]=="int" or table_info[2][i][0:6]=="bigint"):
+            hdfs_column.append({"name":table_info[1][i],"type":table_info[2][i]})
+        else:
+            hdfs_column.append({"name": table_info[1][i], "type": "string"})
+
+    #读取示例json文件
+    with open("venv/dataxJson/example.json", "r") as file:
+        json_data = file.read()
+
+    data = json.loads(json_data)
+    # hdfs
+    if(type=="full"):
+        path ="/origin_data/gmall/app_db/{table_name}_full/dt={date}".format(table_name=table_name,date=datetime.now().strftime("%Y-%m-%d"))
+    elif(type=="inc"):
+        path = "/origin_data/gmall/app_db/{table_name}_inc/dt={date}".format(table_name=table_name,date=datetime.now().strftime("%Y-%m-%d"))
+    else:
+        raise ValueError("Invalid value for 'type'. Expected 'full' or 'inc', got: {}".format(type))
+    # hive
+    # if(type=="full"):
+    #     path ="/user/hive/warehouse/shyl_ods.db/{table_name}_full".format(table_name=table_name)
+    # elif(type=="inc"):
+    #     path = "/user/hive/warehouse/shyl_ods.db/{table_name}_inc".format(table_name=table_name)
+    # else:
+    #     raise ValueError("Invalid value for 'type'. Expected 'full' or 'inc', got: {}".format(type))
+
+    data["job"]["content"][0]["reader"]["parameter"]["connection"][0]["table"]=[table_name]
+    data["job"]["content"][0]["writer"]["parameter"]["column"]=hdfs_column
+    data["job"]["content"][0]["writer"]["parameter"]["path"] = path
+
+    # 输出到文件
+    output_file = f"venv/dataxjob/{table_name}.json".format(table_name=table_name)
+    with open(output_file, "w") as file:
+        json.dump(data, file, indent=4)
+
+    print("Modified data written to", output_file)
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    datax_json("activity_info","full")
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
